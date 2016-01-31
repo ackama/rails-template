aa6b3183b699039b3c0ef27c0c57dd8b9380c769
Josh McArthur <joshua.mcarthur@gmail.com>
2016-02-01 11:46:25 +1300

    Remove usage of base layout

--- app/template.rb
+++ app/template.rb
@@ -10,7 +10,6 @@ end
 copy_file "app/controllers/home_controller.rb"
 copy_file "app/helpers/retina_image_helper.rb"
 copy_file "app/views/layouts/application.html.erb", :force => true
-template "app/views/layouts/base.html.erb.tt"
 copy_file "app/views/shared/_flash.html.erb"
 copy_file "app/views/home/index.html.erb"
 empty_directory_with_keep_file "app/jobs"
