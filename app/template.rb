2793ff9f98c3b889610d2fd73ca1d34629561a8a
Josh McArthur <joshua.mcarthur@gmail.com>
2016-02-01 11:45:58 +1300

    Remove javascript_helper tests

--- app/template.rb
+++ app/template.rb
@@ -8,8 +8,6 @@ insert_into_file "app/controllers/application_controller.rb",
 end
 
 copy_file "app/controllers/home_controller.rb"
-copy_file "app/helpers/javascript_helper.rb"
-copy_file "app/helpers/layout_helper.rb"
 copy_file "app/helpers/retina_image_helper.rb"
 copy_file "app/views/layouts/application.html.erb", :force => true
 template "app/views/layouts/base.html.erb.tt"
