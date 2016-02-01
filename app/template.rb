0e02de8a6701820e484ff86efeff10e9c7efe03e
Josh McArthur <joshua.mcarthur@gmail.com>
2016-02-02 09:57:54 +1300

    Apply template for application layout
    Using a template allows us to access app_const_base and other Rails
    generator helpers.

--- app/template.rb
+++ app/template.rb
@@ -9,7 +9,7 @@ end
 
 copy_file "app/controllers/home_controller.rb"
 copy_file "app/helpers/retina_image_helper.rb"
-copy_file "app/views/layouts/application.html.erb", :force => true
+template "app/views/layouts/application.html.erb", :force => true
 copy_file "app/views/shared/_flash.html.erb"
 copy_file "app/views/home/index.html.erb"
 empty_directory_with_keep_file "app/jobs"
