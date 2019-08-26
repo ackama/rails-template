d20b64a25693e0043c994da83ea7f44bd8fcf937
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-27 10:18:07 +1200

    Wholesale-copy spec folders for system and helpers

--- spec/template.rb
+++ spec/template.rb
@@ -1,11 +1,9 @@
 copy_file "spec/rails_helper.rb", :force => true
 copy_file "spec/spec_helper.rb", :force => true
 copy_file ".rspec"
+directory "spec/system"
+directory "spec/helpers"
 empty_directory_with_keep_file "spec/models"
 empty_directory_with_keep_file "spec/controllers"
-empty_directory_with_keep_file "spec/helpers"
-empty_directory_with_keep_file "spec/system"
 empty_directory_with_keep_file "spec/factories"
 empty_directory_with_keep_file "spec/support"
-copy_file "spec/system/home_feature_spec.rb"
-copy_file "spec/helpers/retina_image_helper_spec.rb"
