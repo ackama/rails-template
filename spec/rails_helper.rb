ee8cb7c782e05ac56a1a5a11d29469b7c375ffdb
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-27 14:31:44 +1200

    Add options for headless Chrome to function inside a Docker container

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,9 +38,13 @@ Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
 
   options.add_argument("--window-size=1920,1080")
-  options.add_argument("--remote-debugging-port=9222")
+  
+
+  # Add other Chrome arguments here
+  
+  # Docker compatibility
+  options.add_argument("--headless")
   options.add_argument("--no-sandbox")
-  options.add_argument("--disable-gpu")
 
   Capybara::Selenium::Driver.new app, browser: :chrome, options: options
 end
