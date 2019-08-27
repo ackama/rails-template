45b217f3f5f4e2849a0cc02feac538ed56a34375
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-27 14:32:08 +1200

    Insert into rails_helper.rb to configure Lighthouse::Matchers

--- variants/accessibility/spec/rails_helper.rb
+++ variants/accessibility/spec/rails_helper.rb
@@ -4,3 +4,11 @@ insert_into_file "spec/rails_helper.rb", after: /require "selenium-webdriver"\n/
     require "axe/rspec"
   REQUIRE
 end
+
+insert_into_file "spec/rails_helper.rb", after: /# Add other Chrome arguments here\n/ do
+  <<-OPTIONS.strip_heredoc
+    # Lighthouse Matcher options
+    options.add_argument("--remote-debugging-port=9222")
+    Lighthouse::Matchers.chrome_flags = %w[headless no-sandbox]
+  OPTIONS
+end
