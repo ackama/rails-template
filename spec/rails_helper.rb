9473b1b468a8aeeaca1a011b02e026b0b37e3796
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-27 12:02:52 +1200

    Don't run headlessly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,6 @@ Capybara.register_driver :chrome do |app|
   options.add_argument("--window-size=1920,1080")
   options.add_argument("--remote-debugging-port=9222")
   options.add_argument("--no-sandbox")
-  options.add_argument("--headless")
   options.add_argument("--disable-gpu")
 
   Capybara::Selenium::Driver.new app, browser: :chrome, options: options
