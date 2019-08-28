364da9c0cb07542cb15afd0aa0d837dc14cbe44f
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-28 16:46:43 +1200

    Add CI for foundation setup

--- template-test/ci-run.sh
+++ template-test/ci-run.sh
@@ -11,14 +11,33 @@ TEST_NAME="basic"
 rm -rf template-test/dummy/$TEST_NAME
 cd template-test/dummy
 
-# Pipe newlines to `rails new` to accept defaults
+# Prompts:
+#   * production hostname
+#   * staging hostname
+#   * no, don't overwrite webpacker.yml with Rails' version
 rails new $TEST_NAME -d postgresql -m $TEMPLATE <<OPTIONS
 example.com
 staging.example.com
 n
 OPTIONS
 
-cd $ROOT
+cd $ROOT && bash template-test/test.sh template-test/dummy/$TEST_NAME
 
-bash template-test/test.sh template-test/dummy/$TEST_NAME
+# Foundation variant
+# Prompts:
+#   * production hostname
+#   * staging hostname
+#   * yes, set up foundation
+#   * no, don't overwrite webpacker.yml with Rails' version
+TEST_NAME="foundation"
+rails new $TEST_NAME -d postgresql -m $TEMPLATE <<OPTIONS
+example.com
+staging.example.com
+y
+n
+OPTIONS
+
+cd $ROOT && bash template-test/test.sh template-test/dummy/$TEST_NAME
+
+
 
