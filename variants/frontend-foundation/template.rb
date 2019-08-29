ee6803fc35d34124ebf2d218c9ab8b20ea1a851c
Josh McArthur <joshua.mcarthur@gmail.com>
2019-08-29 16:05:38 +1200

    Ensure that images directory is created before webpack compilation

--- variants/frontend-foundation/template.rb
+++ variants/frontend-foundation/template.rb
@@ -4,6 +4,7 @@ run "yarn add foundation-sites jquery"
 run "yarn add --dev font-awesome"
 
 directory "app/frontend/foundation"
+empty_directory_with_keep_file "app/frontend/images"
 
 append_to_file "app/frontend/packs/application.js" do
   <<~JS
