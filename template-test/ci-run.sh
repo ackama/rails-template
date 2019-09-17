7ad0bb64a2bf1611cd7348e756f59aebeec13caf
Josh McArthur <joshua.mcarthur@gmail.com>
2019-09-18 10:17:18 +1200

    Experimental support for Travis build matrix

--- template-test/ci-run.sh
+++ template-test/ci-run.sh
@@ -7,40 +7,15 @@ gem install rails --no-document
 mkdir -p template-test/dummy 
 
 # Basic run
-TEST_NAME="basic"
-rm -rf template-test/dummy/$TEST_NAME
+VARIANT="${VARIANT:-basic}"
+rm -rf template-test/dummy/$VARIANT
 cd template-test/dummy
 
-# Prompts:
-#   * production hostname
-#   * staging hostname
-#   * no, don't overwrite webpacker.yml with Rails' version
-rails new $TEST_NAME -d postgresql -m $TEMPLATE <<OPTIONS
-example.com
-staging.example.com
-n
-n
+rails new $VARIANT -d postgresql -m $TEMPLATE <<OPTIONS
+$GENERATOR_INPUT
 OPTIONS
 
-cd $ROOT && bash template-test/test.sh template-test/dummy/$TEST_NAME
-
-cd template-test/dummy
-
-# Foundation variant
-# Prompts:
-#   * production hostname
-#   * staging hostname
-#   * yes, set up foundation
-#   * no, don't overwrite webpacker.yml with Rails' version
-TEST_NAME="foundation"
-rails new $TEST_NAME -d postgresql -m $TEMPLATE <<OPTIONS
-example.com
-staging.example.com
-y
-n
-OPTIONS
-
-cd $ROOT && bash template-test/test.sh template-test/dummy/$TEST_NAME
+cd $ROOT && bash template-test/test.sh template-test/dummy/$VARIANT
 
 
 
