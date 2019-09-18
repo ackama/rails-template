5f711a8177fe7b84c26f27e6d015941a8e45b879
Josh McArthur <joshua.mcarthur@gmail.com>
2019-09-18 15:03:35 +1200

    Add boilerplate logo to provide an example of packing images

--- variants/frontend-foundation-layout/template.rb
+++ variants/frontend-foundation-layout/template.rb
@@ -2,4 +2,12 @@ source_paths.unshift(File.dirname(__FILE__))
 
 apply "app/frontend/foundation/template.rb"
 directory "app/views/application"
+directory "app/frontend/images"
 directory "app/views/home"
+
+gsub_file "app/views/layouts/application.html.erb", "<body>", "<body class=\"no-js\">"
+gsub_file "app/views/layouts/application.html.erb", 
+          "render(\"application/flash\")", 
+          "render(\"application/header/header\")"
+
+insert_into_file "app/views/layouts/application.html.erb", "    <%= render(\"application/footer\") %>", after: "<%= yield %>"
