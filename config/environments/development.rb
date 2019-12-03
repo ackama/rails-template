e28a142dec90e4e55c22b16c8edb9f65f14fc2da
Eoin Kelly <eoin@rabidtech.co.nz>
2019-12-04 06:51:03 +1300

    Use double quotes as per our convention

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,4 +14,4 @@ end
 
 gsub_file "config/environments/development.rb",
           "join('tmp', 'caching-dev.txt')",
-          "join('tmp/caching-dev.txt')"
+          'join("tmp/caching-dev.txt")'
