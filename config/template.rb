4aac9fffda327999386d63bdc99551b82091d8af
Eoin Kelly <eoin@rabidtech.co.nz>
2019-12-03 14:14:13 +1300

    Clean up routes and add workaround for Lighthouse CLI bug to routes

--- config/template.rb
+++ config/template.rb
@@ -7,9 +7,6 @@ copy_file "config/puma.rb", force: true
 remove_file "config/secrets.yml"
 copy_file "config/sidekiq.yml"
 
-gsub_file "config/routes.rb", /  # root 'welcome#index'/ do
-  '  root "home#index"'
-end
 
 copy_file "config/initializers/generators.rb"
 copy_file "config/initializers/rotate_log.rb"
@@ -24,6 +21,39 @@ apply "config/environments/production.rb"
 apply "config/environments/test.rb"
 template "config/environments/staging.rb.tt"
 
-route 'root "home#index"'
-route %Q(mount Sidekiq::Web => "/sidekiq" # monitoring console\n)
-route "require 'sidekiq/web'"
+route <<-EO_ROUTES
+  ##
+  # Workaround a "bug" in lighthouse CLI
+  #
+  # Lighthouse CLI (versions 5.4 - 5.6 tested) issues a `GET /asset-manifest.json`
+  # request during its run - the URL seems to be hard-coded. This file does not
+  # exist so, during tests, your test will fail because rails will die with a 404.
+  #
+  # Lighthouse run from Chrome Dev-tools does not have the same behaviour.
+  #
+  # This hack works around this. This behaviour might be fixed by the time you
+  # read this. You can check by commenting out this block and running the
+  # accessibility and performance tests. You are encouraged to remove this hack
+  # as soon as it is no longer needed.
+  #
+  if Rails.env.test?
+    # manifest paths depend on your webpacker config so we inspect it
+    manifest_path = Webpacker::Configuration
+      .new(root_path: Rails.root, config_path: Rails.root.join("config/webpacker.yml"), env: Rails.env)
+      .public_manifest_path
+      .relative_path_from(Rails.root.join("public"))
+      .to_s
+    get "/asset-manifest.json", to: redirect(manifest_path)
+  end
+
+  ##
+  # If you want the Sidekiq web console in production environments you need to
+  # put it behind some authentication first.
+  #
+  if defined?(Sidekiq::Web) && Rails.env.development?
+    mount Sidekiq::Web => "/sidekiq" # Sidekiq monitoring console
+    require "sidekiq/web"
+  end
+
+  root "home#index"
+EO_ROUTES
