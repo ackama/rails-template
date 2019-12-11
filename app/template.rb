0661daf4a8061eac507a9a62a45d2353ff374c68
Josh McArthur <josh.mcarthur@rabidtech.co.nz>
2019-12-12 08:26:44 +1300

    Resolves Issue #32: Add basic authentication helper method to application controller (#35)
    * Add HTTP basic authentication checks to ApplicationController
    
    * Add commented-out environment variables for controlling HTTP basic authentication

--- app/template.rb
+++ app/template.rb
@@ -1,8 +1,9 @@
 copy_file "app/assets/stylesheets/application.scss"
 remove_file "app/assets/stylesheets/application.css"
 
+template "app/controllers/application_controller.rb", force: true
 copy_file "app/controllers/home_controller.rb"
-template "app/views/layouts/application.html.erb", :force => true
+template "app/views/layouts/application.html.erb", force: true
 copy_file "app/views/application/_flash.html.erb"
 copy_file "app/views/home/index.html.erb"
 
