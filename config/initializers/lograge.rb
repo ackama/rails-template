2dc9b1c9ec3971a41cea4f2561da5efb12085516
Eoin Kelly <eoin@eoinkelly.info>
2020-02-08 20:21:35 +1300

    Add lograge gem to generated Rails apps.
    Rails generates many log lines per request at the 'INFO' level. We want
    one line per successful request in the logs. We cannot achieve this by
    changing to the 'WARN' level because that doesn't generate any output for
    successful requests, hence we are pulling lograge in as a dependency.

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,6 @@
+Rails.application.configure do
+    # lograge can be customised to add custom values to logs, ignore certain
+    # controllers etc. Full instructions at https://github.com/roidrage/lograge
+    config.lograge.enabled = true unless Rails.env.development? || Rails.env.test?
+  end
+end
