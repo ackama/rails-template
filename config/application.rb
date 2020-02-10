2c62a9615e88257b37169ec529a1984658f1d1ae
Eoin Kelly <eoin@eoinkelly.info>
2020-02-10 21:21:15 +1300

    Use a middleware for HTTP basic auth to ensure all routes are protected.
    Before this change we were checking HTTP basic auth in `ApplicationController`
    but this only protects routes which inherit from that controller.

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,26 @@ gsub_file "config/application.rb",
           "config.time_zone = 'Wellington'"
 
 insert_into_file "config/application.rb", before: /^  end/ do
+  # the empty line at the beginning of this string is required
   <<-'RUBY'
 
     # Use sidekiq to process Active Jobs (e.g. ActionMailer's deliver_later)
     config.active_job.queue_adapter = :sidekiq
+  RUBY
+end
+
+insert_into_file "config/application.rb", after: /^require 'rails\/all'/ do
+  # the empty line at the beginning of this string is required
+  <<-'RUBY'
+
+    require_relative '../app/middleware/http_basic_auth'
+  RUBY
+end
+
+insert_into_file "config/application.rb", before: /^  end/ do
+  # the empty line at the beginning of this string is required
+  <<-'RUBY'
+
+    config.middleware.insert_before Rack::Sendfile, HttpBasicAuth
   RUBY
 end
