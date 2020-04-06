f419f6115335d0d209bcbade2bad7bb16be52f03
Eoin Kelly <eoin@eoinkelly.info>
2020-04-06 19:17:32 +1200

    Start using Webpacker for images and SCSS
    * Start using Webpacker for images and SCSS
        * Generate only app/assets/config/manifest.js because the asset pipeline
          requires it
        * Add commented out example of how to reference images to application
          layout.
        * Fix bug where stylesheet_pack_tag was being generated but Webpacker
          was not emitting a CSS file (because none was imported into the pack JS)
        * Demonstrate application.scss loading _elements.scss to encourage devs
          to load all SCSS via @import rather than importing into the pack JS
    * Move the following templates into variants/frontend-base/template.rb
      because the logic was easier to follow (IMHO) in one template:
        * variants/frontend-base/app/template.rb
        * variants/frontend-base/config/template.rb

--- app/template.rb
+++ app/template.rb
@@ -1,14 +1,16 @@
-copy_file "app/assets/stylesheets/application.scss"
-remove_file "app/assets/stylesheets/application.css"
-
+# Configure app/controllers
 copy_file "app/controllers/home_controller.rb"
+
+# Configure app/views
 template "app/views/layouts/application.html.erb", force: true
 copy_file "app/views/application/_flash.html.erb"
 copy_file "app/views/home/index.html.erb"
 
+# Configure app/helpers
 copy_file "app/helpers/retina_image_helper.rb"
 directory "app/middleware"
 
+# Configure empty dirs under app/
 remove_dir "app/jobs"
 empty_directory_with_keep_file "app/workers"
 empty_directory_with_keep_file "app/services"
