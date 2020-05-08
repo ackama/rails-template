0abb10275576c8f6ee9d651262a23203515d89cf
Eoin Kelly <eoin@eoinkelly.info>
2020-05-09 08:45:09 +1200

    Simplify user factory

--- variants/devise/spec/factories/users.rb
+++ variants/devise/spec/factories/users.rb
@@ -1,10 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { "user-#{SecureRandom.hex(5)}@example.com" }
-
-    # User#password is a virtual attribute (i.e. it is not stored direclty in
-    # the DB) so we need call the User#password= method after building the user
-    # to set it.
-    after(:build) { |u| u.password = "aabbccdd" }
+    password { "aabbccdd" }
+    password_confirmation { password }
   end
 end
