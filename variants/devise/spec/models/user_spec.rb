bc056a5efe7e9b074692af777f07d4d99fb9ff9c
Eoin Kelly <eoin@eoinkelly.info>
2020-05-09 08:45:50 +1200

    Add basic tests for User and User factory

--- variants/devise/spec/models/user_spec.rb
+++ variants/devise/spec/models/user_spec.rb
@@ -1,4 +1,15 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  describe "User Factory" do
+    it "creates valid users" do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+  end
+
+  it "is valid when created with valid attributes" do
+    valid_password = "aabbccdd"
+    user = User.new(email: "picard@uss1701d.com", password: valid_password, password_confirmation: valid_password)
+    expect(user).to be_valid
+  end
 end
