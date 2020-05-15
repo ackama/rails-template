48ed911dcf80609a808375e217bafbc88d1eab7a
Josh McArthur <joshua.mcarthur@gmail.com>
2020-05-15 14:34:44 +1200

    Disable redis and mailing healthchecks that are not enabled by default

--- spec/requests/health_checks_spec.rb
+++ spec/requests/health_checks_spec.rb
@@ -4,14 +4,7 @@ require "rails_helper"
 
 RSpec.describe "Automated health checks", type: :request do
   before(:all) { get "/healthchecks/all" }
-
   subject { response.body }
-
   it { is_expected.to include("database: PASSED") }
-
   it { is_expected.to include("default: PASSED") }
-
-  it { is_expected.to include("mailing: PASSED") }
-
-  it { is_expected.to include("redis: PASSED") }
 end
