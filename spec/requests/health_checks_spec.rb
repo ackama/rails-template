ba8ecf35724208265a8d7b54bbf53c6df277d36c
Eoin Kelly <eoin@eoinkelly.info>
2020-06-04 08:28:55 +1200

    Make health check spec pass rubocop-rspec

--- spec/requests/health_checks_spec.rb
+++ spec/requests/health_checks_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe "Automated health checks", type: :request do
-  before(:all) { get "/healthchecks/all" }
+  before(:each) { get "/healthchecks/all" }
   subject { response.body }
   it { is_expected.to include("database: PASSED") }
   it { is_expected.to include("default: PASSED") }
