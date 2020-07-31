021f30e48fea19d7cf6c958de1963309d0bc626b
Eoin Kelly <eoin@eoinkelly.info>
2020-07-31 16:14:57 +1200

    Stop running Overcommit in CI
    CI already runs all our linting so Overcommit is not required here and
    frequently breaks CI because Rubocop adds new cops which generates
    warnings that Overcommit cannot handle.

--- template-test/ci-run.sh
+++ template-test/ci-run.sh
@@ -15,11 +15,6 @@ cd template-test/dummy
 
 echo -e $GENERATOR_INPUT | RACK_ENV=development RAILS_ENV=development rails new $APP_NAME -d postgresql -m $TEMPLATE
 
-# Run overcommit
-cd $APP_NAME
-# gem install overcommit --no-document
-# overcommit -r
-
 cd $ROOT && bash template-test/test.sh template-test/dummy/$APP_NAME
 
 
