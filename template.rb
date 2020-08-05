8d474e8b7e619b88f05e01df5d48c1787f84f194
Dan Morrison <danielroy.morrison@gmail.com>
2020-08-06 08:25:47 +1200

    load performance variant by default when generating new apps

--- template.rb
+++ template.rb
@@ -57,6 +57,9 @@ def apply_template!
 
     # Apply variants after setup and initial install, but before commit
     apply "variants/accessibility/template.rb"
+    # The accessibility template brings in the lighthouse and 
+    # lighthouse matcher parts we need to run performance specs 
+    apply "variants/performance/template.rb"
     apply "variants/frontend-foundation/template.rb" if apply_variant?(:foundation)
     apply "variants/sidekiq/template.rb" if apply_variant?(:sidekiq)
 
