2b0c04e91f89a57cecc8a0d444874d95356aeaf2
Dan Morrison <danielroy.morrison@gmail.com>
2020-08-10 15:16:42 +1200

    Add missing options and overrides to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,23 @@
 
 const config = {
   root: true,
-  env: { commonjs: true },
+  env: { commonjs: true, node: true },
   extends: [
     'ackama',
     'ackama/react'
   ],
   ignorePatterns: ["tmp/"],
+  parserOptions: { sourceType: "module" },
+  overrides: [
+    {
+        files: ["config/webpack/*",
+                "postcss.config.js",
+                "babel.config.js",
+                ".eslintrc.js"
+              ],
+        parserOptions: { sourceType: "script" }
+    }
+  ],
   rules: {}
 };
 
