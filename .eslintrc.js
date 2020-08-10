9def89074f2bc5a7d7ac42cfb3ded2c3d39000e5
Dan Morrison <danielroy.morrison@gmail.com>
2020-08-11 08:57:13 +1200

    apply prettier to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,18 @@
 const config = {
   root: true,
   env: { commonjs: true, node: true, browser: true },
-  extends: [
-    'ackama',
-    'ackama/react'
-  ],
-  ignorePatterns: ["tmp/"],
-  parserOptions: { sourceType: "module" },
+  extends: ['ackama', 'ackama/react'],
+  ignorePatterns: ['tmp/'],
+  parserOptions: { sourceType: 'module' },
   overrides: [
     {
-        files: ["config/webpack/*",
-                "postcss.config.js",
-                "babel.config.js",
-                ".eslintrc.js"
-              ],
-        parserOptions: { sourceType: "script" }
+      files: [
+        'config/webpack/*',
+        'postcss.config.js',
+        'babel.config.js',
+        '.eslintrc.js'
+      ],
+      parserOptions: { sourceType: 'script' }
     }
   ],
   rules: {}
