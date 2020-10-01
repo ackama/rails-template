f1d360df2fb83f41628aa7c6e93780d666abefda
JY <james.yang@ackama.com>
2020-10-01 18:40:22 +1300

    Add example

--- variants/frontend-react/template.rb
+++ variants/frontend-react/template.rb
@@ -8,3 +8,17 @@ run "bundle install"
 run "rails webpacker:install:react"
 run "rails generate react:install"
 run "rails g react:component HelloWorld greeting:string"
+
+
+insert_into_file "app/views/layouts/application.html.erb",
+          after: /<%= javascript_pack_tag \"application\", \"data-turbolinks-track\": \"reload\", defer: true %>\n/ do
+  <<-ERB
+    <%= javascript_pack_tag "application" %>
+  ERB
+end
+
+append_to_file "app/views/home/index.html.erb" do
+  <<~ERB
+    <%= react_component("HelloWorld", { greeting: "Hello from react-rails." }) %>
+  ERB
+end
