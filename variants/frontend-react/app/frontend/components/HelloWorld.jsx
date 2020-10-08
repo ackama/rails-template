ee126d54722e4732d0677a76da7bb06dff8966f6
JY <james.yang@ackama.com>
2020-10-08 16:51:09 +1300

    handle feedback

--- variants/frontend-react/app/frontend/components/HelloWorld.jsx
+++ variants/frontend-react/app/frontend/components/HelloWorld.jsx
@@ -1,13 +1,10 @@
 import * as PropTypes from 'prop-types';
 import React from 'react';
 
-class HelloWorld extends React.Component {
-  render() {
-    return <>Greeting: {this.props.greeting}</>;
-  }
-}
+const HelloWorld = ({ greeting }) => <>Greeting: {greeting}</>;
 
 HelloWorld.propTypes = {
   greeting: PropTypes.string.isRequired
 };
+
 export default HelloWorld;
