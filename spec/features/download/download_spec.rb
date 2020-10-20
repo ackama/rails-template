810ab1abe1fb385f9b7229aefd2126b6136002e3
JY <james.yang@ackama.com>
2020-10-20 14:55:15 +1300

    fix out rubocop errors

--- spec/features/download/download_spec.rb
+++ spec/features/download/download_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "Download page", js: true do
+describe "Download page", type: :feature, js: true do
   before { visit downloadpage_path }
 
-  context "A User (not signed in) on the dowanload page", skip: "Example of download" do
-    it "should show download link" do
+  context "when a User (not signed in) on the download page", skip: "Example of download" do
+    it "show download link" do
       expect(page).to have_link "Download"
     end
 
-    it "should download a example.png file" do
+    it "download a example.png file" do
       click_link "Download"
       DownloadHelpers.clear_downloads
       expect(DownloadHelpers.download_file).to eq("example.png")
