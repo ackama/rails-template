8642cdf6c462e33afbfa25ebd809bba57af47b7a
Gareth Jones <jones258@gmail.com>
2021-05-31 08:09:35 +1200

    feat: support fetching answers from specific env variables

--- variants/frontend-foundation/template.rb
+++ variants/frontend-foundation/template.rb
@@ -22,12 +22,26 @@ end
 def apply_variant?(name)
   return true if ENV.fetch("VARIANTS", "").split(",").include?(name.to_s)
 
-  ask_with_default("Add #{name} to this application?", :blue, 'N').downcase.start_with?("y")
+  ask_with_default(
+    "Add #{name} to this application?",
+    :blue,
+    'N',
+    "RT_APPLY_VARIANT_#{name}".gsub("-", "_").upcase
+  ).downcase.start_with?("y")
 end
 
-def ask_with_default(question, color, default)
+def fetch_answer(question, color, env_variable)
+  env_answer = ENV.fetch(env_variable, nil).to_s.strip
+
+  return ask(question, color) if env_answer.empty?
+
+  say "#{question}: #{env_answer}", color
+  env_answer
+end
+
+def ask_with_default(question, color, default, env_variable)
   question = (question.split("?") << " [#{default}]?").join
-  answer = ask(question, color)
+  answer = fetch_answer(question, color, env_variable)
   answer.to_s.strip.empty? ? default : answer
 end
 
