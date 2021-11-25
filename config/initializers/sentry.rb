bc955bda2a20d912ee9b4c192cb6161ff1c9aeb3
Dave Park <david.park@ackama.com>
2021-11-26 09:56:04 +1300

    Actually set up Sentry correctly. Part 3

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,11 +2,11 @@
 # * https://sentry.io/for/rails/
 # * https://github.com/getsentry/sentry-ruby
 #
-Sentry.configure do |config|
+Sentry.init do |config|
   # Sentry will be enabled if SENTRY_DSN exists. Sentry reporting will work if
   # SENTRY_DSN has a meaningful value.
   config.dsn = ENV["SENTRY_DSN"]
 
   # Set Sentry environment to be current environment if SENTRY_ENV is not set
-  config.current_environment = ENV.fetch("SENTRY_ENV", Rails.env)
+  config.environment = ENV.fetch("SENTRY_ENV", Rails.env)
 end
