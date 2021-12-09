06a31f80c16c3b7d49f458574bded8195e1eb134
Mischa Saunders <mischa.saunders@gmail.com>
2021-12-10 11:13:19 +1300

    Include data as a default image src so that SVGs are enabled automatically

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,7 +11,7 @@ Rails.application.config.content_security_policy do |policy|
   #
   policy.default_src :self
   policy.font_src    :self
-  policy.img_src     :self
+  policy.img_src     :data, :self
   policy.object_src  :none
   policy.script_src  :self
   policy.style_src   :self
