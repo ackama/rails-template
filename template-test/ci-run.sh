c4c18b4b513d6da96961557d9ee6690251c6daf0
emresimtay <emre.simtay@ackama.com>
2022-01-21 13:37:33 +1300

    fix: prevent ci-run from complaining that it cannot find package.json and yarn.lock files and overriding them in the root directory

--- template-test/ci-run.sh
+++ template-test/ci-run.sh
@@ -11,9 +11,16 @@ VARIANT="${VARIANT:-basic}"
 APP_NAME="${VARIANT}-test-app"
 
 rm -rf template-test/dummy/$APP_NAME
+mkdir -p template-test/dummy/$APP_NAME
+
+# otherwise rails webpacker:install initiates yarn init and that overrides these yarn.lock package.json files 
+# exist under the root directory instead of creating new ones under template-test/dummy/$APP_NAME
+cp yarn.lock package.json template-test/dummy/$APP_NAME
+
 cd template-test/dummy
 
-echo -e $GENERATOR_INPUT | RACK_ENV=development RAILS_ENV=development rails new $APP_NAME -d postgresql -m $TEMPLATE
+echo -e $GENERATOR_INPUT | RACK_ENV=development RAILS_ENV=development rails new $APP_NAME -d postgresql -m $TEMPLATE \
+--skip_javascript
 
 cd $ROOT && bash template-test/test.sh template-test/dummy/$APP_NAME
 
