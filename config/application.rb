283a1918c89b83d23814cdf8669216478dc1dcf3
Dave Park <david.park@ackama.com>
2022-03-04 12:09:41 +1300

    Fix: using --skip options fails to generate apps (#278)
    * Fix: application.rb generator after boot. Closes #108 
    
    * Add skips to ci run
    
    * Skip modifying storage.yml if skipping activestorage. Closes #107

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,11 @@ gsub_file "config/application.rb",
           "# config.time_zone = 'Central Time (US & Canada)'",
           "config.time_zone = 'Wellington'"
 
-insert_into_file "config/application.rb", after: /^require ['"]rails\/all['"]/ do
+insert_into_file "config/application.rb", after: /^require_relative ['"]boot['"]/ do
   # the empty line at the beginning of this string is required
   <<-'RUBY'
 
-    require_relative '../app/middleware/http_basic_auth'
+require_relative '../app/middleware/http_basic_auth'
   RUBY
 end
 
