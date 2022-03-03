5ee6d64ff9d360661f5d7be4adf0867ac626f42e
Gareth Jones <jones258@gmail.com>
2022-03-04 09:55:41 +1300

    feat: add react testing library and co

--- variants/frontend-react/app/frontend/components/HelloWorld.jsx
+++ variants/frontend-react/app/frontend/components/HelloWorld.jsx
@@ -1,10 +1,26 @@
 import * as PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 
-const HelloWorld = ({ greeting }) => <>Greeting: {greeting}</>;
+const HelloWorld = ({ initialGreeting }) => {
+  const [greeting, setGreeting] = useState(initialGreeting);
+
+  const updateGreeting = event => {
+    setGreeting(event.target.value);
+  };
+
+  return (
+    <>
+      <b>{greeting}</b>
+      <label htmlFor="greeting-input">
+        Change the greeting
+        <input id="greeting-input" type="text" onChange={updateGreeting} />
+      </label>
+    </>
+  );
+};
 
 HelloWorld.propTypes = {
-  greeting: PropTypes.string.isRequired
+  initialGreeting: PropTypes.string.isRequired
 };
 
 export default HelloWorld;
---
+++ variants/frontend-react/app/frontend/test/components/HelloWorld.spec.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import HelloWorld from '../../components/HelloWorld';
+
+describe('HelloWorld', () => {
+  it('renders the initial greeting', async () => {
+    const { container } = render(<HelloWorld initialGreeting="Hello Ackama" />);
+
+    expect(container).toHaveTextContent(/Hello Ackama/iu);
+  });
+
+  describe('when the user types in a new greeting', () => {
+    it('changes to render that one', async () => {
+      const { container } = render(
+        <HelloWorld initialGreeting="Hello Ackama" />
+      );
+
+      userEvent.type(
+        screen.getByRole('textbox', {
+          name: /change the greeting/iu
+        }),
+        'Hello from the other side'
+      );
+
+      expect(container).toHaveTextContent(/Hello from the other side/iu);
+    });
+  });
+});
