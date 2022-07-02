b036f3c1588b542a433fcb3bac33b1dd8a9ce237
Gareth Jones <jones258@gmail.com>
2022-07-03 08:44:58 +1200

    fix: remove reference to `postcss.config.js` which no longer exists

--- variants/frontend-base/js-lint/fixes.rb
+++ variants/frontend-base/js-lint/fixes.rb
@@ -2,13 +2,6 @@
 # frontend variants have finished running to ensure that we don't cause any
 # overrides or miss files
 
-prepend_to_file "postcss.config.js" do
-  <<~ESLINTFIX
-  /* eslint-disable node/global-require */
-  'use strict'; \n
-  ESLINTFIX
-end
-
 # must be run after prettier is installed and has been configured by setting
 # the 'prettier' key in package.json
 run "yarn run js-lint-fix"
