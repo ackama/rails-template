0249e2def572b6fe6af743de5efa8e23d46e32d2
Eoin Kelly <eoin@eoinkelly.info>
2022-07-20 15:35:50 +1200

    Fix rubocop issue

--- spec/helpers/retina_image_helper_spec.rb
+++ spec/helpers/retina_image_helper_spec.rb
@@ -5,7 +5,7 @@ describe RetinaImageHelper, type: :helper do
 
   before { allow(helper).to receive(:image_path) { |file| "/images/#{file}" } }
 
-  it { assert_match(/\A<img src=".*" srcset=".*" alt=".*" \/>\z/, tag) }
+  it { assert_match(%r{\A<img src=".*" srcset=".*" alt=".*" />\z}, tag) }
   it { assert_match(/alt="example"/, tag) }
   it { assert_match(%r{src="/images/example.png"}, tag) }
   it { assert_match(%r{srcset="/images/example.png 1x,/images/example@2x.png 2x"}, tag) }
