5a27aae9815b20e4649efdd1503688d83fd7abb6
Eoin Kelly <eoin@eoinkelly.info>
2022-08-05 09:51:47 +1200

    Update the Typescript variant to enable images in TS compliant way

--- variants/frontend-typescript/template.rb
+++ variants/frontend-typescript/template.rb
@@ -48,6 +48,20 @@ gsub_file(
   "process.env.SENTRY_ENV ?? process.env.RAILS_ENV"
 )
 
+js_load_images_chunk = <<~EO_JS_ENABLE_IMAGES
+  const images = require.context('../images', true);
+  // eslint-disable-next-line no-unused-vars
+  const imagePath = name => images(name, true);
+EO_JS_ENABLE_IMAGES
+ts_load_images_chunk = <<~EO_TS_ENABLE_IMAGES
+  const images = require.context('../images', true);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const imagePath = (name: string): string => {
+    return String(images(name));
+  };
+EO_TS_ENABLE_IMAGES
+gsub_file("app/frontend/packs/application.ts", js_load_images_chunk, ts_load_images_chunk, force: true)
+
 package_json = JSON.parse(File.read("./package.json"))
 package_json["scripts"]["typecheck"] = "tsc -p . --noEmit"
 File.write("./package.json", JSON.generate(package_json))
