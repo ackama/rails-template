3cb574283653d844e2d415c5782acc29ac7a5932
Gareth Jones <gareth.jones@ackama.com>
2022-08-15 07:25:52 +1200

    test: enable branch coverage tracking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@ SimpleCov.minimum_coverage 90
 SimpleCov.use_merging false
 
 SimpleCov.start do
+  enable_coverage :branch
+  
   add_filter "/bin/"
   add_filter "/lib/tasks/auto_annotate_models.rake"
   add_filter "/lib/tasks/coverage.rake"
