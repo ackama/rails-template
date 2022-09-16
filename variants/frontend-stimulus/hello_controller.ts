49d60d66c6a0c2ece4f56b8f29605657e60cc555
Eoin Kelly <eoin@eoinkelly.info>
2022-09-16 13:55:41 +1200

    Add comment to ts version too

--- variants/frontend-stimulus/hello_controller.ts
+++ variants/frontend-stimulus/hello_controller.ts
@@ -1,5 +1,10 @@
 import { Controller } from '@hotwired/stimulus';
 
+// See https://github.com/orgs/stimulus-components/repositories for examples of
+// Stimulus controllers. Consider whether it is better for the long term health
+// of your app to use these components as a dependency or take some copy & paste
+// inspiration.
+
 // To see this controller in action, add the following to the DOM:
 //
 //   <div data-controller="hello"></div>
