edb4b6fc8e81a40213ab5ab539ee75812586e0b7
Josh McArthur <josh.mcarthur@ackama.com>
2022-09-18 13:35:27 +1200

    Configure I18n to raise an error when translations are missing in test mode (#346)
    * Configure I18n when translations are missing in test mode
    
    Quite a few of our applications use I18n to try and separate content from code. Quite often, we end up accidentally using a fallback translation value, when we intended for an actual value to be present. To defend against that, often our tests will use `I18n.t!` to raise when it can't find a value. I think a more elegant solution would be just to tell ActionView to raise an error at that point when our translations are missing, rather than rely on the test writer to check.
    
    I would be open to adding this to development.rb as well, but I'm unsure how much it would interfere with trying to prototype up a template for a feature.
    
    * Use broader-scoped config.i18n.raise_on_missing_translations = true
    
    * Raise for missing translations in development as well as test
    
    * Appease rubocop
    
    Co-authored-by: Eoin Kelly <eoin.kelly@ackama.com>
    Co-authored-by: Eoin Kelly <eoin@eoinkelly.info>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,5 +11,8 @@ insert_into_file \
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.asset_host = "http://localhost:3000"
+
+  # Raise an error if we try and look up a missing translation
+  config.i18n.raise_on_missing_translations = true
   RUBY
 end
