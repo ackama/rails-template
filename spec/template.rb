53ae7166d3af77c2d0a702097c181a16265718da
Eoin Kelly <eoin@eoinkelly.info>
2022-10-07 08:12:23 +1300

    Generate an empty spec/helpers dir

--- spec/template.rb
+++ spec/template.rb
@@ -3,6 +3,7 @@ copy_file "spec/spec_helper.rb", force: true
 copy_file ".rspec"
 directory "spec/system"
 directory "spec/requests"
+empty_directory_with_keep_file "spec/helpers"
 empty_directory_with_keep_file "spec/models"
 empty_directory_with_keep_file "spec/factories"
 empty_directory_with_keep_file "spec/support"
