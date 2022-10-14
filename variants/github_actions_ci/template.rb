dd8d0d30bf038241bd371bea46757ddd9307b23e
Eoin Kelly <eoin.kelly@ackama.com>
2022-10-15 08:22:53 +1300

    Fix issues with generated CI config (#369)
    * Don't fail to boot if SENTRY_CSP_HEADER_REPORT_ENDPOINT not set
    
    * Set some required ENV vars in Github Actions CI
    
    * Convert CI yml to an ERB template to conditionally load Sidekiq vars

--- variants/github_actions_ci/template.rb
+++ variants/github_actions_ci/template.rb
@@ -1,1 +1,3 @@
-directory("variants/github_actions_ci/workflows", ".github/workflows")
+template "variants/github_actions_ci/workflows/ci.yml.tt", ".github/workflows/ci.yml"
+copy_file "variants/github_actions_ci/workflows/deploy_to_ec2.yml", ".github/workflows/deploy_to_ec2.yml"
+copy_file "variants/github_actions_ci/workflows/deploy_to_heroku.yml", ".github/workflows/deploy_to_heroku.yml"
