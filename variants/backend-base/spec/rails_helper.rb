6216ee10f23af7ff6bc4365bf54ab3034c293b97
Dana Sherson <robot@dana.sh>
2023-04-07 02:32:10 +1200

    Keep tests passing if you have basicauth
    # Conflicts:
    #	.github/workflows/ci.yml

--- variants/backend-base/spec/rails_helper.rb
+++ variants/backend-base/spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
+ENV["HTTP_BASIC_AUTH_USERNAME"] = nil
+ENV["HTTP_BASIC_AUTH_PASSWORD"] = nil
 require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
