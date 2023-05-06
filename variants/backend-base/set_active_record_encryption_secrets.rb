d8f32938c753648bfec645b0a89fb755c876f579
Eoin Kelly <eoin@eoinkelly.info>
2023-05-06 12:51:09 +1200

    Generate separate ActiveRecord encryption secrets for example.env and .env

--- variants/backend-base/set_active_record_encryption_secrets.rb
+++ variants/backend-base/set_active_record_encryption_secrets.rb
@@ -1,4 +1,4 @@
-db_secrets = begin
+def generate_db_secrets
   puts "DB_ENCRYPTION_INIT: Running rails db:encryption:init to set realistic values in ENV variables"
 
   raw = `bin/rails db:encryption:init`
@@ -14,6 +14,15 @@ rescue StandardError => e
   }
 end
 
-gsub_file("example.env", "PLACEHOLDER_PRIMARY_KEY", db_secrets.fetch("primary_key"))
-gsub_file("example.env", "PLACEHOLDER_DETERMINISTIC_KEY", db_secrets.fetch("deterministic_key"))
-gsub_file("example.env", "PLACEHOLDER_KEY_DERIVATION_SALT", db_secrets.fetch("key_derivation_salt"))
+# To avoid setting a bad security example we don't use the same secrets for the
+# example.env (which is checked in) and your local .env file.
+example_env_db_secrets = generate_db_secrets
+dot_env_db_secrets = generate_db_secrets
+
+gsub_file("example.env", "PLACEHOLDER_PRIMARY_KEY", example_env_db_secrets.fetch("primary_key"))
+gsub_file("example.env", "PLACEHOLDER_DETERMINISTIC_KEY", example_env_db_secrets.fetch("deterministic_key"))
+gsub_file("example.env", "PLACEHOLDER_KEY_DERIVATION_SALT", example_env_db_secrets.fetch("key_derivation_salt"))
+
+gsub_file(".env", "PLACEHOLDER_PRIMARY_KEY", dot_env_db_secrets.fetch("primary_key"))
+gsub_file(".env", "PLACEHOLDER_DETERMINISTIC_KEY", dot_env_db_secrets.fetch("deterministic_key"))
+gsub_file(".env", "PLACEHOLDER_KEY_DERIVATION_SALT", dot_env_db_secrets.fetch("key_derivation_salt"))
