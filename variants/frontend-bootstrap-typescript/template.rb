e93eb165d7ee0b373e0e6967572afdf10b93077d
Gareth Jones <gareth.jones@ackama.com>
2023-07-12 11:42:56 +1200

    fix: use latest `prettier` but lock `@typescipt-eslint` to v5 for now (#443)
    * fix: use latest `prettier` as its required by `eslint-plugin-prettier`
    
    * fix: use `@typescript-eslint` v5 for now

--- variants/frontend-bootstrap-typescript/template.rb
+++ variants/frontend-bootstrap-typescript/template.rb
@@ -1,7 +1,10 @@
 source_paths.unshift(File.dirname(__FILE__))
 
 yarn_add_dependencies %w[@types/bootstrap]
-yarn_add_dev_dependencies %w[@typescript-eslint/parser @typescript-eslint/eslint-plugin]
+yarn_add_dev_dependencies %w[
+  @typescript-eslint/parser@5
+  @typescript-eslint/eslint-plugin@5
+]
 
 run "yarn install"
 
