c9fb7660accab5638f49349a3b02051aab0ab35a
Gareth Jones <gareth.jones@ackama.com>
2023-08-15 06:17:46 +1200

    fix: don't include types for `shakapacker` anymore as it ships with types now (#470)
    I upstreamed the types into `shakapacker` itself, which got released in
    v7.0.3: https://github.com/shakacode/shakapacker/pull/330

--- variants/frontend-base-typescript/types.d.ts
+++ variants/frontend-base-typescript/types.d.ts
@@ -1,26 +1,3 @@
-declare module 'shakapacker' {
-  import { Configuration } from 'webpack';
-
-  export { merge } from 'webpack-merge';
-  export const globalMutableWebpackConfig: Configuration;
-  export function generateWebpackConfig(): Configuration;
-}
-
-declare module 'shakapacker/package/babel/preset.js' {
-  import { ConfigAPI, PluginItem, TransformOptions } from '@babel/core';
-
-  interface RequiredTransformOptions {
-    plugins: PluginItem[];
-    presets: PluginItem[];
-  }
-
-  const defaultConfigFunc: (
-    api: ConfigAPI
-  ) => TransformOptions & RequiredTransformOptions;
-
-  export = defaultConfigFunc;
-}
-
 declare module '*.svg' {
   const content: string;
   export default content;
