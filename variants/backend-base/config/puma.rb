238c9a09a28c6cc10688a83fd84d96b2ad76bb36
Eoin Kelly <eoin@eoinkelly.info>
2023-09-22 16:06:08 +1200

    Update puma config to more closely match default Rails 7 config

--- variants/backend-base/config/puma.rb
+++ variants/backend-base/config/puma.rb
@@ -8,6 +8,11 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
 threads min_threads_count, max_threads_count
 
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+
 port ENV.fetch("PORT", 3000)
 environment ENV.fetch("RAILS_ENV", "development")
 
@@ -40,5 +45,5 @@ if workers_count > 1
   end
 end
 
-# Allow puma to be restarted by `rails restart` command.
+# Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
