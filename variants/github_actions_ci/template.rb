89c633c72044c37ebc89bc4f675fbb1ad70a3d12
Gareth Jones <gareth.jones@ackama.com>
2024-01-12 12:39:06 +1300

    feat: run `actionlint` for GitHub Actions variant (#518)
    While rebasing #466 after #516 was landed I learned that I had actually
    mucked up my erb for the GitHub Actions workflow - this was only picked
    up because it was invalid YAML so Prettier errored.
    
    To help avoid mistakes in future, this introduces running
    [`actionlint`](https://github.com/rhysd/actionlint) as part of our
    _template_ CI.
    
    Since doing so means that `bin/ci-run` now includes a tool that isn't in
    our CI workflow, this PR can also be seen as the initial nudge towards a
    possible future with a very different approach to `bin/ci-run`
    (including not having it at all) that I've been wanting to bring up for
    discussion for a bit.

--- variants/github_actions_ci/template.rb
+++ variants/github_actions_ci/template.rb
@@ -1,3 +1,13 @@
 template "variants/github_actions_ci/workflows/ci.yml.tt", ".github/workflows/ci.yml"
 copy_file "variants/github_actions_ci/workflows/deploy_to_ec2.yml", ".github/workflows/deploy_to_ec2.yml"
 copy_file "variants/github_actions_ci/workflows/deploy_to_heroku.yml", ".github/workflows/deploy_to_heroku.yml"
+
+append_to_file "bin/ci-run" do
+  <<~ACTIONLINT
+
+    echo "* ******************************************************"
+    echo "* Running ActionLint"
+    echo "* ******************************************************"
+    actionlint
+  ACTIONLINT
+end
