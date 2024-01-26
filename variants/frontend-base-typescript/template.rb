60f433af460f95d23a2fe78b0f310fea055b78f5
Gareth Jones <jones258@gmail.com>
2024-01-26 13:47:24 +1300

    refactor: replace direct reads of `package.json` with `package_json` usage

--- variants/frontend-base-typescript/template.rb
+++ variants/frontend-base-typescript/template.rb
@@ -53,8 +53,12 @@ ts_load_images_chunk = <<~EO_TS_ENABLE_IMAGES
 EO_TS_ENABLE_IMAGES
 gsub_file!("app/frontend/packs/application.ts", js_load_images_chunk, ts_load_images_chunk, force: true)
 
-update_package_json do |package_json|
-  package_json["scripts"]["typecheck"] = "tsc -p . --noEmit"
+package_json.merge! do |pj|
+  {
+    "scripts" => pj.fetch("scripts", {}).merge({
+      "typecheck" => "tsc -p . --noEmit"
+    })
+  }
 end
 
 append_to_file "bin/ci-run" do
