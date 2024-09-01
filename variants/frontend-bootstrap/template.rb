1903262ffa411cd21af353cd33af58d7e237d9de
Eoin Kelly <eoin.kelly@ackama.com>
2024-09-02 09:43:30 +1200

    Replace deprecated SCSS `@import`with `@use`where possible

--- variants/frontend-bootstrap/template.rb
+++ variants/frontend-bootstrap/template.rb
@@ -8,6 +8,9 @@ insert_into_file "app/frontend/packs/application.js", "import '../js/bootstrap';
 copy_file "app/frontend/stylesheets/customized_bootstrap.scss", force: true
 prepend_to_file "app/frontend/stylesheets/application.scss" do
   <<~EO_CONTENT
+    // We use @import because Bootstrap will not support @use until v6. See
+    // https://github.com/twbs/bootstrap/issues/30025#issuecomment-574825600
+    // Our own SCSS files should use @use.
     @import "../stylesheets/customized_bootstrap";
   EO_CONTENT
 end
