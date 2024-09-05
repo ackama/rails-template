ea1dc8e97e61c369cf993c7722fed4f6043b2130
Eoin Kelly <eoin.kelly@ackama.com>
2024-09-05 13:57:51 +1200

    Move to scss @use from @import where possible (#564)
    Closes #488

--- variants/frontend-bootstrap/template.rb
+++ variants/frontend-bootstrap/template.rb
@@ -6,9 +6,15 @@ copy_file "app/frontend/js/bootstrap.js", force: true
 insert_into_file "app/frontend/packs/application.js", "import '../js/bootstrap';", before: 'import "../stylesheets/application.scss";'
 
 copy_file "app/frontend/stylesheets/customized_bootstrap.scss", force: true
-prepend_to_file "app/frontend/stylesheets/application.scss" do
+append_to_file "app/frontend/stylesheets/application.scss" do
   <<~EO_CONTENT
-    @import "../stylesheets/customized_bootstrap";
+
+    // We use @import because Bootstrap will not support @use until v6. See
+    // https://github.com/twbs/bootstrap/issues/30025#issuecomment-574825600 Our own
+    // SCSS files should use @use. Webpack requires that all `@use` be first in a
+    // file which conflicts with the stylelint no-invalid-position-at-import-rule`
+    // rule so we disable it for this line.
+    @import "../stylesheets/customized_bootstrap"; // stylelint-disable-line no-invalid-position-at-import-rule -- See above
   EO_CONTENT
 end
 
