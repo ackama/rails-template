bf0656105a57323b1fc3d2629e990f688908cbc5
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-02 13:19:51 +1200

    fix: don't both setting chrome flags

--- variants/accessibility/spec/rails_helper.rb
+++ variants/accessibility/spec/rails_helper.rb
@@ -9,7 +9,6 @@ insert_into_file! "spec/rails_helper.rb", after: /# Add other Chrome arguments h
   <<~OPTIONS
     # Lighthouse Matcher options
     options.add_argument("--remote-debugging-port=9222")
-    Lighthouse::Matchers.chrome_flags = %w[headless=new no-sandbox]
     Lighthouse::Matchers.remote_debugging_port = 9222
     Lighthouse::Matchers.results_directory = Rails.root.join("tmp/lighthouse")
   OPTIONS
