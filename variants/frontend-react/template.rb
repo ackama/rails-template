84f3989012215aee76357169ec2935751dec4c10
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-12-07 09:56:52 +1300

    fix: actually lock `connection_pool` to v2

--- variants/frontend-react/template.rb
+++ variants/frontend-react/template.rb
@@ -2,7 +2,8 @@ source_paths.unshift(File.dirname(__FILE__))
 
 TERMINAL.puts_header "Adding react-rails to Gemfile"
 
-gem "react-rails", "~> 2.0"
+gem "react-rails"
+gem "connection_pool", "~> 2.0"
 run "bundle install"
 
 run "rails generate react:install"
