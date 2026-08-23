7b6486a99e0ed678b3ed9692f0a91e4f79431361
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-08-24 08:19:47 +1200

    feat: remove performance variant (#686)
    I (and others) don't think we've ever actually gotten a measurable
    result from the performance tests, as it's just too sensitive in
    differences between environments, and we've ended up removing it in our
    latest projects

--- variants/performance/spec/performance/home_page_spec.rb
+++
@@ -1,7 +0,0 @@
-require "performance_helper"
-
-RSpec.describe "Home page", type: :system do
-  before { visit root_path }
-
-  it_behaves_like "a performant page"
-end
--- variants/performance/template.rb
+++
@@ -1,6 +0,0 @@
-source_paths.unshift(File.dirname(__FILE__))
-
-directory "spec/performance"
-copy_file "spec/performance/home_page_spec.rb"
-copy_file "spec/support/shared_examples/a_performant_page.rb"
-copy_file "spec/performance_helper.rb"
